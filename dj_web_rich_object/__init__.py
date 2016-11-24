16aaa62c7e791d30385da9787467e88c93569502
ZuluPro <montheanthony@hotmail.com>
2016-11-24 10:21:27 -0500

    Added profile widget

--- dj_web_rich_object/__init__.py
+++ dj_web_rich_object/__init__.py
@@ -1,5 +1,5 @@
 """Reusable app made for handle web rich objects"""
-VERSION = (0, 1, 6)
+VERSION = (0, 1, 7)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Anthony Monthe (ZuluPro)'
 __email__ = 'anthony.monthe@gmail.com'
